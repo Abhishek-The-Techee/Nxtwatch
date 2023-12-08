6bd443709d3ca0beebf85b0aae9ce788907669bd
Abhishek <abhishekktech2@gmail.com>
2023-12-08 09:31:50 +0000

    Header Completed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,9 @@
+import {withRouter} from 'react-router-dom'
 import {BsMoon, BsBrightnessHigh} from 'react-icons/bs'
 import {GiHamburgerMenu} from 'react-icons/gi'
+import Popup from 'reactjs-popup'
+import Cookies from 'js-cookie'
+import {FiLogOut} from 'react-icons/fi'
 
 import ThemeAndVideoContext from '../../context/ThemeAndVideoContext'
 
@@ -10,6 +14,13 @@ import {
   ThemeBtn,
   ProfileImg,
   MenuIcon,
+  LogoutBtn,
+  ModalContainer,
+  ModalDescription,
+  ButtonsContainer,
+  ConfirmBtn,
+  CancelBtn,
+  LogoutIconBtn,
 } from './styledComponents'
 
 const Header = () => (
@@ -18,6 +29,7 @@ const Header = () => (
       const {isDarkTheme, toggleTheme} = value
       const bgColor = isDarkTheme ? '#000000' : '#ffffff'
       const color = isDarkTheme ? '#ffffff' : '#1e293b'
+      const buttonColor = isDarkTheme ? '#ffffff' : ' #3b82f6'
 
       const logoImage = isDarkTheme
         ? 'https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-dark-theme-img.png'
@@ -31,6 +43,12 @@ const Header = () => (
 
       const onClickChangeTheme = () => {
         toggleTheme()
+      }
+
+      const onClickLogout = props => {
+        const {history} = props
+        Cookies.remove('jwt_token')
+        history.replace('/login')
       }
 
       return (
@@ -47,10 +65,58 @@ const Header = () => (
             <MenuIcon>
               <GiHamburgerMenu size={30} color={color} />
             </MenuIcon>
+            <Popup
+              modal
+              trigger={
+                <LogoutBtn bgColor={bgColor} color={buttonColor}>
+                  Logout
+                </LogoutBtn>
+              }
+            >
+              {close => (
+                <ModalContainer>
+                  <ModalDescription>
+                    Are you sure, you want to logout?
+                    <ButtonsContainer>
+                      <CancelBtn type="button" onClick={() => close()}>
+                        Cancel
+                      </CancelBtn>
+                      <ConfirmBtn type="button" onClick={onClickLogout}>
+                        Confirm
+                      </ConfirmBtn>
+                    </ButtonsContainer>
+                  </ModalDescription>
+                </ModalContainer>
+              )}
+            </Popup>
+            <Popup
+              modal
+              trigger={
+                <LogoutIconBtn bgColor={bgColor} color={buttonColor}>
+                  <FiLogOut size={30} color={color} />
+                </LogoutIconBtn>
+              }
+            >
+              {close => (
+                <ModalContainer>
+                  <ModalDescription>
+                    Are you sure, you want to logout?
+                    <ButtonsContainer>
+                      <CancelBtn type="button" onClick={() => close()}>
+                        Cancel
+                      </CancelBtn>
+                      <ConfirmBtn type="button" onClick={onClickLogout}>
+                        Confirm
+                      </ConfirmBtn>
+                    </ButtonsContainer>
+                  </ModalDescription>
+                </ModalContainer>
+              )}
+            </Popup>
           </RightMenuOptionsContainer>
         </NavbarContainer>
       )
     }}
   </ThemeAndVideoContext.Consumer>
 )
-export default Header
+export default withRouter(Header)
--- src/components/Header/styledComponents.js
+++ src/components/Header/styledComponents.js
@@ -28,14 +28,96 @@ export const ProfileImg = styled.img`
   width: 30px;
   margin-left: 20px;
   margin-right: 20px;
-  @media screen and (max-width: 576px) {
+  @media screen and (max-width: 768px) {
     display: none;
   }
 `
 export const MenuIcon = styled.div`
   margin-left: 10px;
   margin-right: 10px;
-  @media screen and (min-width: 576px) {
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`
+export const LogoutBtn = styled.button`
+  height: 34px;
+  width: 80px;
+  font-family: 'Roboto';
+  color: ${props => props.color};
+  background-color: ${props => props.bgColor};
+  border-color: ${props => props.color};
+  border: 1px solid;
+  border-radius: 2px;
+  font-size: 16px;
+  font-weight: bold;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`
+export const ModalContainer = styled.div`
+  height: 180px;
+  width: 350px;
+  border-radius: 8px;
+  background-color: #e2e8f0;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  @media screen and (max-width: 768px) {
+    height: 150px;
+    width: 250px;
+  }
+`
+export const ModalDescription = styled.p`
+  font-family: 'Roboto';
+  color: #000000;
+  font-weight: 500;
+  font-size: 18px;
+  text-align: center;
+  @media screen and (min-width: 768px) {
+    padding-top: 20px;
+  }
+`
+export const ButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 20px;
+  @media screen and (min-width: 768px) {
+    margin-top: 50px;
+  }
+`
+export const ConfirmBtn = styled.button`
+  background-color: #3b82f6;
+  color: #ffffff;
+  height: 36px;
+  width: 90px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  font-weight: 450;
+`
+
+export const CancelBtn = styled.button`
+  background-color: transparent;
+  color: #000000;
+  height: 36px;
+  width: 90px;
+  border: 1px solid #000000;
+  outline: none;
+  cursor: pointer;
+  font-weight: 450;
+`
+export const LogoutIconBtn = styled.button`
+  background-color: transparent;
+  color: #000000;
+  height: 36px;
+  width: 90px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  font-weight: 450;
+  @media screen and (min-width: 768px) {
     display: none;
   }
 `
