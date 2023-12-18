46d2523850d8e8b58d55ac029b567409c8d939f8
Abhishek <abhishekktech2@gmail.com>
2023-12-18 06:58:15 +0000

    Working on Home Route

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,91 @@
 import {Component} from 'react'
+
+import {AiOutlineClose, AiOutlineSearch} from 'react-icons/ai'
+
 import Header from '../Header'
 import Navbar from '../SideNavbar'
 
+import ThemeAndVideoContext from '../../context/ThemeAndVideoContext'
+
+import {
+  HomeContainer,
+  BannerContainer,
+  BannerLeftContainer,
+  BannerLogo,
+  BannerHeading,
+  BannerBtn,
+  BannerRightContainer,
+  BannerCloseBtn,
+  SearchContainer,
+  SearchInputBox,
+  SearchBtn,
+} from './styledComponents'
+
 class Home extends Component {
+  state = {
+    displayBanner: 'flex',
+    searchInput: '',
+  }
+
+  onCloseBanner = () => {
+    this.setState({displayBanner: 'none'})
+  }
+
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
   render() {
+    const {searchInput, displayBanner} = this.state
+
     return (
-      <>
-        <Header />
-        <Navbar />
-      </>
+      <ThemeAndVideoContext.Consumer>
+        {value => {
+          const {isDarkTheme} = value
+          const bgColor = isDarkTheme ? '#000000' : '#ffffff'
+          const textColor = isDarkTheme ? '#ffffff' : '#000000'
+          const closeBanner = displayBanner === 'flex' ? 'flex' : 'none'
+
+          return (
+            <>
+              <Header />
+              <Navbar />
+              <HomeContainer bgColor={bgColor}>
+                <BannerContainer display={closeBanner}>
+                  <BannerLeftContainer>
+                    <BannerLogo
+                      src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
+                      alt="nxt watch logo"
+                    />
+                    <BannerHeading>
+                      Buy Nxt Watch Premium prepaid plans with UPI
+                    </BannerHeading>
+                    <BannerBtn>GET IT NOW</BannerBtn>
+                  </BannerLeftContainer>
+                  <BannerRightContainer>
+                    <BannerCloseBtn onClick={this.onCloseBanner}>
+                      <AiOutlineClose size={25} />
+                    </BannerCloseBtn>
+                  </BannerRightContainer>
+                </BannerContainer>
+                {/* Search Container */}
+                <SearchContainer>
+                  <SearchInputBox
+                    type="search"
+                    placeholder="Search"
+                    value={searchInput}
+                    onChange={this.onChangeSearchInput}
+                    color={textColor}
+                  />
+                  <SearchBtn>
+                    <AiOutlineSearch size={25} />
+                  </SearchBtn>
+                </SearchContainer>
+              </HomeContainer>
+            </>
+          )
+        }}
+      </ThemeAndVideoContext.Consumer>
     )
   }
 }
---
+++ src/components/Home/styledComponents.js
@@ -0,0 +1,95 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.div`
+  background-color: ${props => props.bgColor};
+  height: 100vh;
+  overflow-y: auto;
+  padding-top: 60px;
+`
+export const BannerContainer = styled.div`
+  background-image: url('https://assets.ccbp.in/frontend/react-js/nxt-watch-banner-bg.png');
+  height: 250px;
+  width: 85%;
+  padding: 20px;
+  background-size: cover;
+  margin-left: 206px;
+
+  display: ${props => props.display};
+  @media screen and (max-width: 768px) {
+    margin-left: 0px;
+    background-size: cover;
+    width: 100%;
+  }
+`
+export const BannerLeftContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+`
+export const BannerLogo = styled.img`
+  height: 36px;
+  width: 180px;
+  @media screen and (max-width: 768px) {
+    height: 30px;
+    width: 100px;
+  }
+`
+export const BannerHeading = styled.h1`
+  font-family: 'Roboto';
+  font-weight: 430;
+  font-size: 24px;
+  padding-top: 15px;
+  color: #1e293b;
+  @media screen and (max-width: 768px) {
+    font-size: 18px;
+    padding-top: 15px;
+  }
+`
+export const BannerBtn = styled.button`
+  height: 36px;
+  width: 130px;
+  background-color: transparent;
+  color: #1e293b;
+  font-family: 'Roboto';
+  font-weight: 430;
+  font-size: 16px;
+  outline: none;
+  border: 1px solid #000000;
+  margin-top: 40px;
+`
+export const BannerRightContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  width: 50%;
+`
+export const BannerCloseBtn = styled.button`
+  border: none;
+  background-color: transparent;
+  height: 25px;
+`
+export const SearchContainer = styled.div`
+  display: flex;
+  margin: 20px;
+`
+export const SearchInputBox = styled.input`
+  height: 38px;
+  width: 400px;
+  outline: none;
+  margin-left: 206px;
+  padding: 5px;
+  border: 1px solid #94a3b8;
+  color: ${props => props.color};
+  font-family: 'Roboto';
+  background-color: transparent;
+  @media screen and (max-width: 768px) {
+    margin-left: 20px;
+  }
+`
+export const SearchBtn = styled.button`
+  height: 38px;
+  width: 80px;
+  background-color: #f4f4f4;
+  outline: none;
+  border: 1px solid #94a3b8;
+`
